e2b5697985f651d77068b9ac65e8a1746bc390fe
Alena <Slivinskaa2002@gmail.com>
2022-12-15 17:39:49 +0600

    add img

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
 import List from "./List";
+import logo from './For_dummies.png';
+import dont_know from './dont_know.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={logo} alt="piechart" height="300px" width="300px"></img>
        <List />
+       <img src={dont_know} alt="piechart" height="300px" width="300px"></img>
       </header>
     </div>
   );
